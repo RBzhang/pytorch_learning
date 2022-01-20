330721ad1ef948b9d653480f3fd121e88134f0b7
zhangkun <zhangkun3365@gmail.com>
2022-01-20 18:13:16 +0800

    CNN2

--- convolutional_Neural.py
+++ convolutional_Neural.py
@@ -4,12 +4,19 @@ width, height = 100 , 100
 kernel_size = 3
 batch_size = 1
 
-input = torch.randn(batch_size, in_channels, width, height)
+input = [3,4,6,5,7,2,4,6,8,2,1,6,7,8,4,9,7,4,6,2,3,7,5,4,1]
 
-conv_layer = torch.nn.Conv2d(in_channels, out_channels, 3)
+input = torch.Tensor(input).view(1,1,5,5)
+
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size= 3, stride= 2 ,padding=1, bias = False)
+
+kernel = torch.Tensor([1,2,3,4,5,6,7,8,9]).view(1,1,3,3)
+
+conv_layer.weight.data = kernel.data
 
 output = conv_layer(input)
 
 print(input.shape)
 print(output.shape)
 print(conv_layer.weight.shape)
+print(output.data)
