a3323a37a961db1b2193dc3e5f0a9623f318ddc6
zhangkun <zhangkun3365@gmail.com>
2022-01-20 19:37:25 +0800

    CNN4

--- convolutional_Neural.py
+++ convolutional_Neural.py
@@ -1,22 +1,75 @@
 import torch
-in_channels, out_channels = 5, 10
-width, height = 100 , 100
-kernel_size = 3
-batch_size = 1
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision import datasets
+#from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch.optim as optim
+batch_size = 64
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307),(0.3081))
+])
 
-input = [3,4,6,5,7,2,4,6,8,2,1,6,7,8,4,9,7,4,6,2,3,7,5,4,1]
+train_dataset = datasets.MNIST(root='~/dataset/mnist',train=True,download=True,transform = transform)
 
-input = torch.Tensor(input).view(1,1,5,5)
+train_loader = DataLoader(train_dataset,shuffle=True,batch_size=batch_size)
 
-conv_layer = torch.nn.Conv2d(1, 1, kernel_size= 3, stride= 2 ,padding=1, bias = False)
+test_dataset = datasets.MNIST(root='~/dataset/mnist',train=False,download=True,transform = transform)
 
-kernel = torch.Tensor([1,2,3,4,5,6,7,8,9]).view(1,1,3,3)
+test_loader = DataLoader(test_dataset,shuffle=True,batch_size=batch_size)
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size = 5)
+        self.conv2 = torch.nn.Conv2d(10,20,kernel_size=5)
+        self.pooling = torch.nn.MaxPool2d(2)
+        self.linear1 = torch.nn.Linear(320,256)
+        self.linear2 = torch.nn.Linear(256,64)
+        self.linear3 = torch.nn.Linear(64,10)
+    
+    def forward(self,x):
+        batch_size = x.size(0)
+        x = F.relu(self.pooling(self.conv1(x)))
+        x = F.relu(self.pooling(self.conv2(x)))
+        x = x.view(batch_size, -1)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+model = Net()
 
-conv_layer.weight.data = kernel.data
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.01 , momentum = 0.5)
 
-output = conv_layer(input)
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx , data in enumerate(train_loader,0):
+        inputs, target = data
+        optimizer.zero_grad()
 
-print(input.shape)
-print(output.shape)
-print(conv_layer.weight.shape)
-print(output.data)
+        outputs = model(inputs)
+        loss = criterion(outputs , target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %0.3f' % (epoch + 1 , batch_idx + 1 , running_loss / 300))
+            running_loss = 0.0
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data , dim = 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy on testt set: %d %%'  %  (100 * correct / total))
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
